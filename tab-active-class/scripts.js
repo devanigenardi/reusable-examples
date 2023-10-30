cc93eb6f73a0093efb9ca8edd26fdbf4f1d370b9
devanigenardi <devani.genardi.estrada@gmail.com>
2023-10-30 15:56:42 -0600

    new tabs code

--- tab-active-class/scripts.js
+++ tab-active-class/scripts.js
@@ -1,22 +1,19 @@
-function openTab(tabId) {
-    // Get all tab buttons and tab content elements
-    const tabButtons = document.querySelectorAll('.tab-button');
-    const tabContents = document.querySelectorAll('.tab-content');
+const listOfTabs = document.querySelectorAll(".tab");
+const tabsContainer = document.querySelector('.tabs-container');
+const panels = document.querySelectorAll('.panel');
 
-    // Hide all tab content elements
-    tabContents.forEach(tabContent => {
-        tabContent.style.display = 'none';
+tabsContainer.addEventListener('click',(ele) => {
+    const clicked = ele.target;
+
+    panels.forEach((pan) => {
+        pan.classList.remove("visible")
     });
 
-    // Remove the 'active' class from all tab buttons
-    tabButtons.forEach(tabButton => {
-        tabButton.classList.remove('active');
+    listOfTabs.forEach((item) => {
+        item.classList.remove("active")
     });
+    
+    clicked.classList.add("active");
 
-    // Show the selected tab content
-    document.getElementById(tabId).style.display = 'block';
-
-    // Add the 'active' class to the clicked tab button
-    const activeTabButton = document.querySelector(`[onclick="openTab('${tabId}')"]`);
-    activeTabButton.classList.add('active');
-}
+    document.querySelector(`.content-${clicked.dataset.tab}`).classList.add("visible")
+})
